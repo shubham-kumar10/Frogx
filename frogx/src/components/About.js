f2997265bfb58afa0fff58b245a96b569c9914be
Farid Sayyed <faridsayyed1010@gmail.com>
2022-10-26 23:40:50 +0530

    React project

---
+++ frogx/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum donor amet consectetfhg siujhsdvjh gasdhOAG U IUS
+            Lorem ipsum donor amet consectetfhg siujhsdvjh gasdhOAG U IUS CC.  CC.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
---
+++ frogx/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum donor amet consectetfhg siujhsdvjh gasdhOAG U IUS  CC.</p>
+        </div>
+    )
+}
+
+export default Contact;
