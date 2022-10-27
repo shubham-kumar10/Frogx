04cb7271ba4143b203b33c1f4d85c19e6cf391a2
Shubham Kumar <shubham.kumar4@go-mmt.com>
2022-10-27 23:35:17 +0530

    fix: router fix

--- frogx/src/App.js
+++ frogx/src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Post from './components/Post';
-
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Post from "./components/Post";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
       <div className="App">
         <Navbar />
         <Switch>
-        <Route  path='/' component={Home} />
-        <Route path='/about'   component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route path="//:post_id" component={Post} />
+          <Route exact path="/" component={About} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="//:post_id" component={Post} />
         </Switch>
       </div>
-      </BrowserRouter>
     );
   }
 }
-   
+
 export default App;
